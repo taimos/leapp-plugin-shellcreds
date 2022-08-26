0341d11e0ab67d5d1f2811e813c00f79aa9b453b
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2022-08-26 02:32:16 +0200

    chore: migrate to projen type

--- .projenrc.js
+++ .projenrc.js
@@ -1,18 +1,15 @@
-const { typescript, javascript, github } = require('projen');
-const project = new typescript.TypeScriptProject({
+const { LeappPluginProject } = require('@taimos/projen-leapp-plugin');
+const { javascript, github } = require('projen');
+
+const project = new LeappPluginProject({
   defaultReleaseBranch: 'main',
   name: 'leapp-shellcreds',
   description: 'Plugin to copy AWS credentials in a shell compatible format',
-  keywords: ['leapp-plugin'],
   deps: [
-    '@noovolari/leapp-core',
     'clipboardy',
   ],
   devDeps: [
-    // 'esbuild',
-    'ts-loader',
-    'webpack',
-    'webpack-cli',
+    '@taimos/projen-leapp-plugin',
   ],
   authorName: 'Taimos GmbH',
   authorEmail: 'info@taimos.de',
@@ -51,30 +48,6 @@ const project = new typescript.TypeScriptProject({
 
 * **Other information**:`],
 });
-
-project.gitignore.exclude('plugin.js');
-project.package.addField('files', ['plugin.js']);
-project.package.addField('leappPlugin', {
-  supportedOS: [
-    'mac',
-    'windows',
-    'linux',
-  ],
-  supportedSessions: [
-    'awsIamRoleFederated',
-    'awsIamRoleChained',
-    'awsSsoRole',
-    'awsIamUser',
-    'aws',
-  ],
-});
-
-project.projectBuild.compileTask.reset('webpack --config webpack.config.js');
-// project.projectBuild.compileTask.reset('esbuild --bundle src/index.ts --outfile=plugin.js --format=cjs --platform=node'); //--minify --sourcemap');
-
-const installTask = project.addTask('deploy');
-installTask.spawn(project.buildTask);
-installTask.exec('mkdir -p ~/.Leapp/plugins/leapp-shellcreds && cp plugin.js package.json ~/.Leapp/plugins/leapp-shellcreds/');
 
 project.gitpod.addCustomTask({
   init: 'yarn install --check-files --frozen-lockfile',
